3baaca1a2219c726e37f7f734b2de0dd512103bb
wendellavila <wendelljcavila@gmail.com>
2024-08-23 17:03:20 -0300

    refactor: Renamed converter functions

--- lib/converter.py
+++ lib/converter.py
@@ -5,8 +5,8 @@ import numpy as np
 def line_string_to_points(line_string: LineString) -> list:
     return np.vstack(line_string.coords.xy).T.tolist()
 
-def to_wgs84(gdf: GeoDataFrame) -> GeoDataFrame:
+def coordinates_to_wgs84(gdf: GeoDataFrame) -> GeoDataFrame:
     return gdf.to_crs(epsg=4326)
 
-def to_line_string(gdf: GeoDataFrame) -> GeoDataFrame:
+def split_multipart(gdf: GeoDataFrame) -> GeoDataFrame:
     return gdf.explode()
