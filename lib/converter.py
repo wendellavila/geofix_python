0bebd24ac98294702e143fda44433ad74270ed8e
wendellavila <wendelljcavila@gmail.com>
2024-08-23 16:25:20 -0300

    feat: Convert MultiLineString to LineString

--- lib/converter.py
+++ lib/converter.py
@@ -2,3 +2,6 @@ from geopandas import GeoDataFrame
 
 def to_wgs84(gdf: GeoDataFrame) -> GeoDataFrame:
     return gdf.to_crs(epsg=4326)
+
+def to_line_string(gdf: GeoDataFrame) -> GeoDataFrame:
+    return gdf.explode()
