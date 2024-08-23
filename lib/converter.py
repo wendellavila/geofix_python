2882d92a788a640f7927038ad9c72740ce2366da
wendellavila <wendelljcavila@gmail.com>
2024-08-23 16:58:59 -0300

    feat: Added util for converting LineStrings to latlng list

--- lib/converter.py
+++ lib/converter.py
@@ -1,4 +1,9 @@
 from geopandas import GeoDataFrame
+from shapely import LineString
+import numpy as np
+
+def line_string_to_points(line_string: LineString) -> list:
+    return np.vstack(line_string.coords.xy).T.tolist()
 
 def to_wgs84(gdf: GeoDataFrame) -> GeoDataFrame:
     return gdf.to_crs(epsg=4326)
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from lib import file_handler
 from lib import converter
+from lib import connector
 
 gdf = file_handler.read_file("samples/lines/retas_fragmentadas.shp")
 gdf = converter.to_wgs84(gdf)
 gdf = converter.to_line_string(gdf)
 
-print(gdf)
+line_string = gdf.at[0, 'geometry']
+print(connector.line_string_to_points(line_string))
 
-file_handler.write_file(gdf)
+# print(gdf)
+# file_handler.write_file(gdf)
