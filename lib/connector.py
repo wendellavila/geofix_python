02dd94127365bd4c049932a70a274145fb83b160
wendellavila <wendelljcavila@gmail.com>
2024-08-25 09:48:38 -0300

    chore: Updated .gitignore, removed unused methods

--- lib/connector.py
+++ lib/connector.py
@@ -1,16 +1,5 @@
 from geopandas import GeoDataFrame
 from lib import converter
 
-def has_lines(gdf: GeoDataFrame) -> bool:
-    line_string = gdf.at[0, 'geometry']
-    points = converter.line_string_to_points(line_string)
-    return True if len(points) == 2 else False
-
-def connect_lines(gdf: GeoDataFrame) -> GeoDataFrame:
-    return gdf
-
-def connect_curves(gdf: GeoDataFrame) -> GeoDataFrame:
-    return gdf
-
 def connect_gaps(gdf: GeoDataFrame) -> GeoDataFrame:
-    return connect_lines(gdf) if has_lines(gdf) else connect_curves(gdf) 
+    return gdf
